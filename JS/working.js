6db3721ac2318a58f1345967094123dc493ea245
sadik-mahdi <sadikmahadi06@gmail.com>
2026-02-24 22:28:34 +0600

    first commit

---
+++ JS/working.js
@@ -0,0 +1,8 @@
+document.getElementById("select-all").addEventListener('click',function(){
+  const allJobCards = document.getElementsByClassName('job-card');
+  const totalCount = allJobCards.length;
+console.log(totalCount);
+})
+document.getElementById("select-interview").addEventListener('click',function(){
+  
+})
