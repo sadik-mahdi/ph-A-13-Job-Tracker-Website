4b3cdacd5c65f2fe9417e4ba2f99faef29719b3b
sadik-mahdi <sadikmahadi06@gmail.com>
2026-02-24 23:56:52 +0600

    added a few flexibility

--- JS/working.js
+++ JS/working.js
@@ -1,8 +1,31 @@
 document.getElementById("select-all").addEventListener('click',function(){
+ 
+  const button = document.getElementById("select-all");
+  button.onclick = function(){
+    interviewBtn.classList.remove('bg-white', 'text-[#64748B]');
+    interviewBtn.classList.add('bg-blue-700', 'text-white');
+  }
+  window.location.replace("index.html");
+
   const allJobCards = document.getElementsByClassName('job-card');
   const totalCount = allJobCards.length;
-console.log(totalCount);
+  console.log(totalCount);
 })
 document.getElementById("select-interview").addEventListener('click',function(){
-  
+  const button = document.getElementById("select-interview");
+  button.onclick = function(){
+    interviewBtn.classList.remove('bg-white', 'text-[#64748B]');
+    interviewBtn.classList.add('bg-blue-700', 'text-white');
+  }
+
+  document.getElementById("job-cards").style.display = "none";
+})
+document.getElementById("select-rejected").addEventListener('click',function(){
+  const button = document.getElementById("select-reject");
+  button.onclick = function(){
+    interviewBtn.classList.remove('bg-white', 'text-[#64748B]');
+    interviewBtn.classList.add('bg-blue-700', 'text-white');
+  }
+
+  window.location.replace("dashboard.html");
 })
